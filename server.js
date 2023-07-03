a43891619a884019ab2390e91f216490d976def2
trace-martin <trace.martin7817@gmail.com>
2023-07-03 10:36:59 -0600

    initial commit/server creation

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost/social_network_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// Define routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/thoughts', require('./routes/thoughtRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
